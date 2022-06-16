01c3c4fd4a828337db1ce863e9c40eb2f61b8726
Imogen Hare <imogenbhare@gmail.com>
2022-06-16 11:26:58 +0100

    candidate list outline added but not teste d using MM + candidate id and candidate name need to be updated to the actual object keys

--- client/my-app/src/lists/candidatesList.jsx
+++ client/my-app/src/lists/candidatesList.jsx
@@ -3,17 +3,17 @@ import { NavLink } from 'react-router-dom';
 import { Box } from 'rebass'
 import { Tiles } from '@rebass/layout'
 
-export default function TokensList(props) {
+export default function CandidatesList(props) {
 
   return (
     <Box px={2} py={2} width={2 / 3}>
       <Tiles width={[96, null, 128]}>
-        {props.tokens.map( token => 
+        {props.cadidates.map( candidate => 
           // need to replace the _id with what the actual thing is
-          <div key={token.token_id}> 
+          <div key={candidate.candidate_id}> 
           {/* need to change the "token_name" to the actual object key */}
           {/* here will we will also need to add an image or something but let's start with the name */}
-            <NavLink to={`elections/${token.token_id}`}>{token.token_name}</NavLink>
+            <NavLink to={`candidate/${candidate.candidate_id}`}>{candidate.candidate_name}</NavLink>
           </div>
           )}
       </Tiles>
